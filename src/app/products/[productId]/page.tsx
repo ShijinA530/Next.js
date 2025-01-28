6e2510e9d36081bde20378873180a7c7b4716d55
ShijinA530 <shijinasasi835@gmail.com>
2025-01-28 20:46:57 +0530

    initial commmit

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,23 @@
-function ProductDetails({
-    params,
-}: {
-    params:{productId:string}
-}) {
+import { Metadata } from "next"
+
+type Props = {
+  params: {
+    productId: string
+  }
+}
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const title = await new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`)
+    }, 100)
+  })
+  return {
+    title: `Product ${title}`
+  }
+}
+
+function ProductDetails({params}: Props) {
   return (
     <div>
       <h1>Details about product {params.productId}</h1>
